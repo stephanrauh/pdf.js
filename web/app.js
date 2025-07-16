edc358eb572b5022d83775bc10e334a159b03b53
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-07-16 21:51:48 +0200

    stephanrauh/ngx-extended-pdf-viewer#2943 fixed a bug that confused the page order when clicking the "move page up" button

--- web/app.js
+++ web/app.js
@@ -1430,7 +1430,7 @@ appConfig: null,
     this.pdfViewer.swapPages(event.source.pageNumber - 1, event.source.pageNumber - 2);
     this.pdfThumbnailViewer.swapThumbnails(event.source.pageNumber - 1, event.source.pageNumber - 2);
     this.page = event.source.pageNumber - 1;
-    [this.pageOrder[event.source.pageNumber - 1], this.pageOrder[event.source.pageNumber]] = [this.pageOrder[event.source.pageNumber], this.pageOrder[event.source.pageNumber - 1]];
+    [this.pageOrder[event.source.pageNumber - 2], this.pageOrder[event.source.pageNumber - 1]] = [this.pageOrder[event.source.pageNumber - 1], this.pageOrder[event.source.pageNumber - 2]];
   },
 
   movePageDown(event) {
