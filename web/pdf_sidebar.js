946866d50c6ee6052d9fb1e18945080f6e5a02c6
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-09-11 21:27:00 +0200

    stephanrauh/ngx-extended-pdf-viewer#3016 activate the sidebar resizer

--- web/pdf_sidebar.js
+++ web/pdf_sidebar.js
@@ -512,11 +512,11 @@ class PDFSidebar {
   }
 
   #mouseMove(evt) {
-    let width = evt.clientX;
-    // For sidebar resizing to work correctly in RTL mode, invert the width.
-    if (this.#isRTL) {
-      width = this.outerContainerWidth - width;
-    }
+    // Get the mouse position relative to the outerContainer
+    const containerRect = this.outerContainer.getBoundingClientRect();
+
+    const width = this.#isRTL ? containerRect.right - evt.clientX : evt.clientX - containerRect.left;
+
     this.#updateWidth(width);
   }
 
